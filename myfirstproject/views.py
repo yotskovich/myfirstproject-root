4a8f59ca97642b0c820668f927f78698c6c05858
yotskovich <yatskevichaliaksei.11@gmail.com>
2023-01-15 20:04:07 +0100

    First commit

---
+++ myfirstproject/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def about(request):
+    return HttpResponse("This is about page")
+
+def home(request):
+    return render(request,"home.html", {'greeting': 'hello'})
+
+def main(request):
+    return render(request,"django.html")
